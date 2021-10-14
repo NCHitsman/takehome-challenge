56732da3b5cc3663001ef17d2b2d864c77579b79
Noah Carmichael-Hitsman <noah.hitsman@gmail.com>
2021-10-14 14:26:21 -0700

    added email verification and error

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -53,7 +53,7 @@ const EmailTextAndInput = ({ email, setEmail }) => {
       <div className='InputLabelText'>Email*</div>
       <input
         className='EmailInput FormInput'
-        type='email'
+        type='text'
         value={email}
         onChange={(e) => setEmail(e.target.value)}
         placeholder='Email'
@@ -131,14 +131,22 @@ const App = ({ db }) => {
   const [year, setYear] = useState("Year");
   const [sucessful, setSucessful] = useState(false);
 
-  const namesChecker = () => {
+  const namesAndEmailChecker = () => {
     const currentNameErrors = [];
-    if (!firstName.length || !lastName.length) {
+    const emailCheckExpression = /(?!.*\.{2})^([a-z\d!#$%&'*+\-\/=?^_`{|}~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]+(\.[a-z\d!#$%&'*+\-\/=?^_`{|}~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]+)*|"((([ \t]*\r\n)?[ \t]+)?([\x01-\x08\x0b\x0c\x0e-\x1f\x7f\x21\x23-\x5b\x5d-\x7e\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|\\[\x01-\x09\x0b\x0c\x0d-\x7f\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))*(([ \t]*\r\n)?[ \t]+)?")@(([a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|[a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF][a-z\d\-._~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]*[a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])\.)+([a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|[a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF][a-z\d\-._~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]*[a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])\.?$/i; 
+    if (!firstName.length || !lastName.length || !email.length) {
       if (!firstName.length) {
-        currentNameErrors.push('- Please add a first name.')
+        currentNameErrors.push('- Please add your first name.')
       }
       if (!lastName.length) {
-        currentNameErrors.push('- Please add a last name.')
+        currentNameErrors.push('- Please add your last name.')
+      }
+      if (!email.length) {
+        currentNameErrors.push('- Please add your email.')
+      } else {
+        if (!emailCheckExpression.test(email)) {
+          currentNameErrors.push('- Please add a valid email.')
+        }
       }
       setNameErrors(currentNameErrors)
       return false
@@ -172,14 +180,15 @@ const App = ({ db }) => {
     event.preventDefault()
 
     let datesResult = datesChecker()
-    let namesResult = namesChecker()
+    let namesAndEmailResult = namesAndEmailChecker()
 
-    if (datesResult && namesResult) {
+    if (datesResult && namesAndEmailResult) {
       db.putItem('First Name', firstName);
       db.putItem('Middle Name', middleName ? middleName : null);
       db.putItem('Last Name', lastName);
+      db.putItem('Email', email);
       db.putItem("Date of Birth", `${month}/${day}/${year}`);
-      setSucessful(true)
+      setSucessful(true);
     }
   }
 
