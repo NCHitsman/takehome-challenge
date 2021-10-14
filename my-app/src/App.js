1085cde387a52d53742c90ce8853ef0d12c56438
Noah Carmichael-Hitsman <noah.hitsman@gmail.com>
2021-10-14 12:19:02 -0700

    planning out state variables

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,6 +1,17 @@
+import { useState } from 'react';
 import './App.css';
 
 function App(db) {
+  const [nameErrors, setNameErrors] = useState([]);
+  const [dateErrors, setDateErrors] = useState([]);
+  const [firstName, setFirstName] = useState("");
+  const [middleName, setMiddleName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [day, setDay] = useState("Day");
+  const [month, setMonth] = useState("Month");
+  const [year, setYear] = useState("Year");
+  const [sucessful, setSucessful] = useState(false);
+
   return (
     <div className="App">
 
