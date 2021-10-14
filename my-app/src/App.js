86eece81eeb45e0de9a9bb872073ceac891c2a74
Noah Carmichael-Hitsman <noah.hitsman@gmail.com>
2021-10-14 12:27:27 -0700

    add full legal name text and start submit handler

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -13,11 +13,19 @@ function App(db) {
   const [year, setYear] = useState("Year");
   const [sucessful, setSucessful] = useState(false);
 
+  const submitHandler = (event) => {
+    event.preventDefault()
+  }
+
   return (
     <div className="ParentAppDiv">
       <img className="Img" src={image} alt="Img"></img>
       <div className="WelcomeText">Welcome!</div>
       <div className='WelcomeSubText'>Lest's start with some basic info</div>
+
+      <form className='FormParent' onSubmit={submitHandler}>
+        <div className='FormFullLegalText'>Full Legal Name</div>
+      </form>
     </div>
   );
 }
