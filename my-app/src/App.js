441e11e3290dd478203b36265db9149970fed10f
Noah Carmichael-Hitsman <noah.hitsman@gmail.com>
2021-10-14 14:29:08 -0700

    moved date arrays to make collapsing a little nicer

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -62,16 +62,17 @@ const EmailTextAndInput = ({ email, setEmail }) => {
   )
 }
 
-let months = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
-let daysArray = [];
-let yearsArray = [];
-for (let i = 0; i < 31; i++) {
-  daysArray.push(i + 1);
-}
-for (let i = 2021; i >= 2021 - 99; i--) {
-  yearsArray.push(i);
-}
 const DateOfBirthInput = ({ month, setMonth, day, setDay, year, setYear, dateErrors }) => {
+  let months = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
+  let daysArray = [];
+  let yearsArray = [];
+  for (let i = 0; i < 31; i++) {
+    daysArray.push(i + 1);
+  }
+  for (let i = 2021; i >= 2021 - 99; i--) {
+    yearsArray.push(i);
+  }
+
   return (
     <>
       <div className="FormTitleText">Date of Birth*</div>
@@ -133,7 +134,7 @@ const App = ({ db }) => {
 
   const namesAndEmailChecker = () => {
     const currentNameErrors = [];
-    const emailCheckExpression = /(?!.*\.{2})^([a-z\d!#$%&'*+\-\/=?^_`{|}~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]+(\.[a-z\d!#$%&'*+\-\/=?^_`{|}~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]+)*|"((([ \t]*\r\n)?[ \t]+)?([\x01-\x08\x0b\x0c\x0e-\x1f\x7f\x21\x23-\x5b\x5d-\x7e\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|\\[\x01-\x09\x0b\x0c\x0d-\x7f\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))*(([ \t]*\r\n)?[ \t]+)?")@(([a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|[a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF][a-z\d\-._~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]*[a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])\.)+([a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|[a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF][a-z\d\-._~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]*[a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])\.?$/i; 
+    const emailCheckExpression = /(?!.*\.{2})^([a-z\d!#$%&'*+\-\/=?^_`{|}~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]+(\.[a-z\d!#$%&'*+\-\/=?^_`{|}~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]+)*|"((([ \t]*\r\n)?[ \t]+)?([\x01-\x08\x0b\x0c\x0e-\x1f\x7f\x21\x23-\x5b\x5d-\x7e\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|\\[\x01-\x09\x0b\x0c\x0d-\x7f\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))*(([ \t]*\r\n)?[ \t]+)?")@(([a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|[a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF][a-z\d\-._~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]*[a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])\.)+([a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|[a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF][a-z\d\-._~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]*[a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])\.?$/i;
     if (!firstName.length || !lastName.length || !email.length) {
       if (!firstName.length) {
         currentNameErrors.push('- Please add your first name.')
